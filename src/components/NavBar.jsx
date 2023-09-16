8b253adc78d8a42732a75c9bb2620f0c9b3e6830
Yusuf <ysfayds41@gmail.com>
2023-09-16 13:15:41 +0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+       <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="">Home</Nav.Link>
+            <NavLink to="/product-list">Product List</NavLink>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,17 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "../../src/components/NavBar";
 import Main from "../../src/pages/Main";
 import NewProduct from "../../src/pages/NewProduct";
+import ProductList from "../../src/pages/ProductList";
+import About from "../../src/pages/About";
+import UpdateProduct from "../../src/pages/UpdateProduct";
+import NotFound from "../../src/pages/NotFound";
 
-const AppRouer = () => {
+const AppRouter = () => {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="" element={<Main/>}/>
         <Route path="/new-product" element={<NewProduct/>}/>
+        <Route path="/product-list" element={<ProductList/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/update-product" element={<UpdateProduct/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default AppRouer;
+export default AppRouter;
